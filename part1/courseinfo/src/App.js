4f899d611e6f9dd6624ca7b22c9d59c48e3703c3
Hien Tran <hien.tptran@gmail.com>
2022-10-14 15:46:52 +0200

    Added unicafe

--- part1/courseinfo/src/App.js
+++ part1/courseinfo/src/App.js
@@ -1,42 +1,52 @@
 import React from "react"
 
 const App = () => {
-  const course = 'Half Stack application development'
-  const part1 = 'Fundamentals of React'
-  const exercises1 = 10
-  const part2 = 'Using props to pass data'
-  const exercises2 = 7
-  const part3 = 'State of a component'
-  const exercises3 = 14
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
+  }
 
   return (
     <div>
       <Header course={course} />
-      <Content part1={part1} part2={part2} part3={part3} ex1={exercises1} ex2={exercises2} ex3={exercises3} />
-      <Total ex1={exercises1} ex2={exercises2} ex3={exercises3}/>
+      <Content course={course} />
+      <Total course={course}/>
     </div>
   )
 }
 
 const Header = (props) => {
   return (
-    <h1>{props.course}</h1>
+    <h1>{props.course.name}</h1>
   )
 }
 
 const Content = (props) => {
   return (
     <div>
-      <Part part={props.part1} ex={props.ex1}/>
-      <Part part={props.part2} ex={props.ex2}/>
-      <Part part={props.part3} ex={props.ex3}/>
+      <Part part={props.course.parts[0].name} ex={props.course.parts[0].exercises}/>
+      <Part part={props.course.parts[1].name} ex={props.course.parts[1].exercises}/>
+      <Part part={props.course.parts[2].name} ex={props.course.parts[2].exercises}/>
     </div>
   )
 }
 
 const Total = (props) => {
   return (
-    <div>Total number of exercises: {props.ex1 + props.ex2 + props.ex3}</div>
+    <div>Total number of exercises: {props.course.parts[0].exercises + props.course.parts[1].exercises + props.course.parts[2].exercises}</div>
   )
 }
 
